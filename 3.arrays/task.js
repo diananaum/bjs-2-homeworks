38c286d4bedb49f85a16235018129d2c7c303471
Diana Vakhitova <dianaum0809@gmail.com>
2023-02-14 13:10:31 +0300

    Array task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every(index => arr1[index] === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, arr) => {
+        acc += age
+        if(index === arr.length - 1) {
+            return acc / arr.length;
+        } 
+        return acc; 
+    }, 0);
+    return result;
 }
